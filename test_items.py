71e43afe11a39125286aa61ec39f8bd9b237545d
Olga Solodko <jasminess847@gmail.com>
2022-10-11 17:31:36 +0300

    add message for assert

--- test_items.py
+++ test_items.py
@@ -8,4 +8,4 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_add_item_btn_is_on_the_page(browser):
     browser.get(link)
 
-    assert browser.find_element(By.CSS_SELECTOR, '.btn.btn-add-to-basket')
+    assert browser.find_element(By.CSS_SELECTOR, '.btn.btn-add-to-basket'), 'There is no element \'add to basket\' on the page'
