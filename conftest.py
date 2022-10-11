0d63bd9648bdebc64dae8f7f982113226691a9b9
Olga Solodko <jasminess847@gmail.com>
2022-10-11 17:19:12 +0300

    fix test&update conftest

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -9,10 +10,9 @@ def pytest_addoption(parser):
 @pytest.fixture()
 def browser(request):
     language = request.config.getoption('language')
-    language = None
-    if language == 'en-gb':
-        browser = webdriver.Chrome()
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
     yield browser
-    print('\nquit browser...'
-    )
+    print('\nquit browser...')
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,10 +1,11 @@
-import pytest
 from selenium.webdriver.common.by import By
+import time
 
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_add_item_btn(browser):
+
+def test_add_item_btn_is_on_the_page(browser):
     browser.get(link)
-    btn = browser.find_element(By.CSS_SELECTOR, '.btn.btn-add-to-basket')
-    
+
+    assert browser.find_element(By.CSS_SELECTOR, '.btn.btn-add-to-basket')
