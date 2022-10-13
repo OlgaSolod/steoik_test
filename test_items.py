c83bfd5e03b43418cd4639a7b082f87d7afa5e9d
Olga <jasminess847@gmail.com>
2022-10-13 10:14:10 +0300

    update README & fix test_items

--- test_items.py
+++ test_items.py
@@ -7,5 +7,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_add_item_btn_is_on_the_page(browser):
     browser.get(link)
-
-    assert browser.find_element(By.CSS_SELECTOR, '.btn.btn-add-to-basket'), 'There is no element \'add to basket\' on the page'
+    btn = browser.find_elements(By.CSS_SELECTOR, '.btn.btn-add-to-basket1' )
+    assert btn, 'There is no element \'add to basket\' on the page'
